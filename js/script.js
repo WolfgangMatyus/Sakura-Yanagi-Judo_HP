4fed1707c93298c72d9e264b8704ed03fa5911f1
Matyus Wolfgang <wi21b055@technikum-wien.at>
2023-09-24 15:26:03 +0200

    img banner footer head

--- js/script.js
+++ js/script.js
@@ -1,4 +1,54 @@
-function load() {
-    alert('works!');
-    console.log('works!');
+function build(){
+    //loadHead();
+    loadBody();
+    //loadFooter()
+}
+
+
+function loadHead() {
+
+    $.ajax({
+        url: '../component/head.html',
+        type: 'GET',
+        dataType: 'html',
+        success: function(data) {
+          $('#html').html(data);
+        },
+        error: function() {
+          //alert('Fehler beim Laden der Datei.');
+          window.location.href = 'error.html';
+        }
+      });
+}
+
+function loadBody() {
+
+    $.ajax({
+        url: '../component/body.html',
+        type: 'GET',
+        dataType: 'html',
+        success: function(data) {
+          $('#content').html(data);
+        },
+        error: function() {
+          //alert('Fehler beim Laden der Datei.');
+          window.location.href = 'error.html';
+        }
+      });
+}
+
+function loadFooter() {
+
+    $.ajax({
+        url: '../component/footer.html',
+        type: 'GET',
+        dataType: 'html',
+        success: function(data) {
+          $('#footer').html(data);
+        },
+        error: function() {
+          //alert('Fehler beim Laden der Datei.');
+          window.location.href = 'error.html';
+        }
+      });
 }
