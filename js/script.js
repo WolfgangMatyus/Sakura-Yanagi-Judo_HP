32acecc1672c29fa1a9d7387aa702d6c46b75e4a
WolfgangMatyus <wi21b055@technikum-wien.at>
2023-10-31 15:45:59 +0100

    31.10.2023

--- js/script.js
+++ js/script.js
@@ -13,13 +13,13 @@ function loadContent(target) {
         dataType: 'html',
         success: function(data) {
           $('#content').html(data);
+          changeActive(target);
         },
         error: function() {
           //alert('Fehler beim Laden der Datei.');
           window.location.href = 'error.html';
         }
-      });
-
+      });      
 }
 
 function loadBanner() {
@@ -90,8 +90,60 @@ function e() {
   e.preventDefault();
   var offcanvas = new bootstrap.Offcanvas(document.getElementById('staticBackdrop'));
   offcanvas.toggle();
-};
+}
 
 function sendResetPasswordEmail() {
   alert("EMAIL GESENDET")
 }
+
+function changeActive(target){
+  //console.log(target);
+  active = document.getElementById("navbar-nav")
+  console.log(active);
+}
+
+window.addEventListener("scroll", function() {
+  var header = document.querySelector("header");
+  if (window.scrollY > 10) {
+    header.classList.add("header-collapsed"); // Füge die CSS-Klasse für den verkleinerten Header hinzu
+  } else {
+    header.classList.remove("header-collapsed"); // Entferne die CSS-Klasse, um den Header zurückzusetzen
+  }
+});
+
+
+/* ---- SLIDESHOW-GALLERY ---- */
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+/* ---- SLIDESHOW-GALLERY ---- ENDE ---- */
